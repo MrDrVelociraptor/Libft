419f9ad02d6bfbcf6893032467787954f338efa8
Nicholas Speedy <nspeedy@F3C025.42adel.org.au>
2021-09-10 08:59:04 +0930

    first commit

---
+++ .vimrc
@@ -0,0 +1,8 @@
+set nocompatible
+filetype on
+filetype plugin on
+filetype indent on
+syntax on
+set number
+set cursorline
+set cursorcolumn
---
+++ libft.h
@@ -0,0 +1,19 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(const char *s);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+char	*ft_strchr(const char *s, int c);	
+char	*ft_strrchr(const char *s, int c);
+void	*ft_memset(void *dest, int c, size_t len);
+void	ft_bzero(void *dest, size_t n);
+
+#endif
