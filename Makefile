5fd5d24b3e03ccaff9179c9ff4e09ec2d7fc3ecd
Nicholas Speedy <nspeedy@F2N013.42adel.org.au>
2021-09-10 17:02:19 +0930

    added more

--- Makefile
+++ Makefile
@@ -24,8 +24,8 @@ SRCS = ft_isalpha.c \
 	ft_memcpy.c \
 	ft_memmove.c \
 	ft_strlcpy.c \
-#	ft_strlcat.c \
-#	ft_strncmp.c \
+	ft_strlcat.c \
+	ft_strncmp.c \
 #	ft_memchr.c \
 #	ft_memcmp.c \
 #	ft_strnstr.c \
--- libft.h
+++ libft.h
@@ -18,5 +18,7 @@ void	ft_bzero(void *dest, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t destlen);
+size_t  ft_strlcat(char *dest, const char *src, size_t size);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
