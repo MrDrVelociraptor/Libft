90316ea883872f9caa1c8c0c2ba9011f6f6ac3a3
Nicholas Speedy <nspeedy@F3C010.42adel.org.au>
2021-09-24 18:05:27 +0930

    Working versions in all testers of split and subs, VICTORYYYYY

--- ft_split.c
+++ ft_split.c
@@ -5,54 +5,66 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nspeedy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/17 15:47:18 by nspeedy           #+#    #+#             */
-/*   Updated: 2021/09/17 18:10:55 by nspeedy          ###   ########.fr       */
+/*   Created: 2021/09/24 11:21:06 by nspeedy           #+#    #+#             */
+/*   Updated: 2021/09/24 17:44:40 by nspeedy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h"
 #include	<stdio.h>
 
-static size_t	ft_wordlen(char const *s, char c)
+static int	ft_wordlen(char const *s, char c)
 {
-	size_t	len;
+	int	len;
 
 	len = 0;
-	while (*s && *s++ != c)
+	while (*s == c)
+		s++;
+	while (*s && *s != c)
+	{
+		s++;
 		len++;
+	}
 	return (len);
 }
 
-static size_t	ft_wordcount(char const *s, char c)
+static int	ft_find_words(char const *s, char c)
 {
-	size_t	count;
+	int	words;
 
-	count = 0;
+	words = 0;
 	while (*s && *s == c)
 		s++;
 	while (*s)
 	{
-		count++;
 		while (*s && *s != c)
 			s++;
 		while (*s && *s == c)
 			s++;
+		words++;
 	}
-	return (count);
+	return (words);
 }
 
-static char	*ft_strndup(const char *s, size_t n)
+static char	*ft_strndup(const char *s, int n)
 {
+	int		i;
 	char	*result;
 
 	result = (char *)malloc(sizeof(char) * (n + 1));
 	if (!result)
 		return (0);
-	result = ft_memcpy(result, s, n);
+	i = 0;
+	while (i < n)
+	{
+		result[i] = s[i];
+		i++;
+	}
+	result[i] = 0;
 	return (result);
 }
 
-static char	**ft_free_split(char **s, int i)
+static char	**ft_freee(char **s, int i)
 {
 	while (i--)
 		free(s[i]);
@@ -63,23 +75,25 @@ static char	**ft_free_split(char **s, int i)
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
-	size_t	count;
-	size_t	wordlen;
-	size_t	i;
+	int		count;
+	int		wordlen;
+	int		i;
 
 	if (!s)
 		return (0);
-	i = 0;
-	count = ft_wordcount(s, c);
+	count = ft_find_words(s, c);
 	result = (char **)malloc(sizeof(char *) * (count + 1));
 	if (!result)
-		return (ft_free_split(result, i));
+		return (0);
+	i = 0;
 	while (i < count)
 	{
 		while (*s && *s == c)
 			s++;
 		wordlen = ft_wordlen(s, c);
 		result[i] = ft_strndup(s, wordlen);
+		if (!result)
+			return (ft_freee(result, i));
 		s += wordlen;
 		i++;
 	}
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nspeedy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 16:25:59 by nspeedy           #+#    #+#             */
-/*   Updated: 2021/09/14 17:52:43 by nspeedy          ###   ########.fr       */
+/*   Updated: 2021/09/24 18:03:16 by nspeedy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*ft_substr(char	const *s, unsigned int start, size_t len)
 {
 	char	*subs;
 
+	if ((unsigned int)ft_strlen(s) < start)
+		return (ft_strdup(""));
 	subs = (char *)malloc(sizeof(char) * (len + 1));
 	if (!subs)
 		return (0);
