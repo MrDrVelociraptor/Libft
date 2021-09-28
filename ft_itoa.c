083ac0963d0c265c0031b27c4ed686b38e23de37
Nicholas Speedy <nspeedy@F2N020.42adel.org.au>
2021-09-28 17:01:47 +0930

    Fixed itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,51 +6,52 @@
 /*   By: nspeedy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 17:58:46 by nspeedy           #+#    #+#             */
-/*   Updated: 2021/09/27 15:51:07 by nspeedy          ###   ########.fr       */
+/*   Updated: 2021/09/28 17:00:59 by nspeedy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h"
+#include	<stdio.h>
 
 static size_t	ft_numnum(int n)
 {
 	size_t	len;
 
-	len = 1;
-	if (n < 0)
+	len = 0;
+	if (n <= 0)
 		len++;
-	while (n != 0)
+	while (n)
 	{
-		n /= 10;
 		len++;
+		n /= 10;
 	}
 	return (len);
 }
 
 char	*ft_itoa(int n)
 {
-	char		*output;
-	size_t		len;
-	size_t		negpos;
+	char			*result;
+	size_t			len;
+	unsigned int	nbr;
 
+	nbr = (unsigned int)n;
 	len = ft_numnum(n);
-	negpos = 1;
-	output = (char *)malloc(sizeof(char) * (len + 1));
-	if (!output)
+	result = (char *)malloc(sizeof(char) * len + 1);
+	if (!result)
 		return (0);
-	output[--len] = '\0';
-	if (n == 0)
-		output[0] = '0';
 	if (n < 0)
 	{
-		negpos *= -1;
-		output[0] = '-';
+		result[0] = '-';
+		nbr *= -1;
 	}
-	while (n != 0)
+	if (nbr == 0)
+		result[0] = '0';
+	result[len--] = '\0';
+	while (nbr)
 	{
+		result[len] = nbr % 10 + '0';
+		nbr /= 10;
 		len--;
-		output[len] = ((n % 10) * negpos) + '0';
-		n /= 10;
 	}
-	return (output);
+	return (result);
 }
